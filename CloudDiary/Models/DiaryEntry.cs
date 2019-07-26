3fe665244a2180e065d98d3111f3e9881709d845
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-07-26 17:13:40 -0500

    Added my Diary Entry model

---
+++ CloudDiary/Models/DiaryEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudDiary.Models
+{
+    public class DiaryEntry
+    {
+        public DateTime CreatedDate { get; set; }
+        public string Text { get; set; }
+    }
+}
