96c81b03a34014b8fadc41e19b061cc4ecde1819
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-07-25 23:31:06 -0500

    User must now login in order to view diary page - no more security hole

--- CloudDiary/Controllers/DiaryController.cs
+++ CloudDiary/Controllers/DiaryController.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CloudDiary.Controllers
 {
     public class DiaryController : Controller
     {
+        //GET: Diary
+        //user MUST be logged in in order to view this method
+        [Authorize]
         public IActionResult Index()
         {
             return View("Diary");
