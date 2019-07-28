a4d6b50de8b7503e73f73f3b717bd9d9daeaa80a
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-07-27 22:10:38 -0500

    just added a view model - still need to pass to the controller

---
+++ CloudDiary/Models/AddDiaryEntryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudDiary.Models
+{
+    public class AddDiaryEntryViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        [DisplayName("New Entry")]
+        public string Text { get; set; }
+    }
+}
