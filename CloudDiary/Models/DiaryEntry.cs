465d9281fc4788ddefab5e42a714198ca619e6be
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-07-29 18:19:03 -0500

    fixed model class for database

--- CloudDiary/Models/DiaryEntry.cs
+++ CloudDiary/Models/DiaryEntry.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +9,18 @@ namespace CloudDiary.Models
 {
     public class DiaryEntry
     {
+        //global unique identifier as primary key
+        public Guid Id { get; set; }
+
         public DateTime Created { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Text { get; set; }
+
+        public string UserId { get; set; }
+
+        //no applicationUser class?
+        public IdentityUser User { get; set; }
     }
 }
