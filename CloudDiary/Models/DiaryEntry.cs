f699a4aafb76a2505f1a14c3268a472a371a3936
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-07-30 16:12:51 -0500

    slight change from yesterday seeing where git puts it

--- CloudDiary/Models/DiaryEntry.cs
+++ CloudDiary/Models/DiaryEntry.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,6 +21,7 @@ namespace CloudDiary.Models
 
         public string UserId { get; set; }
 
+        [ForeignKey("UserId")]
         public IdentityUser User { get; set; }
     }
 }
