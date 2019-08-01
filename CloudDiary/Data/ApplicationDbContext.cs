c193062ec08fdc07d31f5c5b2334667ef27bec65
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-07-31 23:43:11 -0500

    Fixed my onModelCreating error by calling the base within the override butstill having an error accessing/adding to the database

--- CloudDiary/Data/ApplicationDbContext.cs
+++ CloudDiary/Data/ApplicationDbContext.cs
@@ -22,6 +22,7 @@ namespace CloudDiary.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
