c6a5d5580f1205c1ccc5c3e6a4ca04e05180a49b
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-08-04 17:57:21 -0500

    Installed the correct packages once again and targeted the entityframeworkCore as opposed to default that I cant figure out yet how to uninstall and successfully migrated my new model

---
+++ CloudDiary/Data/Migrations/20190804225601_addedFKToEntry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CloudDiary.Data.Migrations
+{
+    public partial class addedFKToEntry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiaryEntries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Text = table.Column<string>(maxLength: 100, nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiaryEntries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DiaryEntries_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiaryEntries_UserId",
+                table: "DiaryEntries",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiaryEntries");
+        }
+    }
+}
