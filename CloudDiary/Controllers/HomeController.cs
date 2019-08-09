1fcf97f542dc5f533322107b1434a4ace3fc1c2e
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-08-08 22:30:51 -0500

    Moving things areounsd to experiment with UX - made the button in home oindex to lead to login mov to correct link after moving controller methods

--- CloudDiary/Controllers/HomeController.cs
+++ CloudDiary/Controllers/HomeController.cs
@@ -14,75 +14,85 @@ namespace CloudDiary.Controllers
 {
     public class HomeController : Controller
     {
-        //GET: Diary
-        //user MUST be logged in in order to view this method
-        [Authorize]
-        public IActionResult Index()
-        {
-            //this is simply how I created test data
-            //var diaryEntries = new List<DiaryEntry>();
-            //diaryEntries.Add(new DiaryEntry() { Created = DateTime.Now, Text = "Just feel like coding today haha" });
-            //diaryEntries.Add(new DiaryEntry() { Created = DateTime.Now.AddDays(-1),
-            //    Text = "Rained all day great thats a happy person lolol" });
-            //diaryEntries.Add(new DiaryEntry() { Created = DateTime.Now.AddDays(-2), Text = "What a fun project" });
+        ////GET: Diary
+        ////user MUST be logged in in order to view this method
+        //[Authorize]
+        //public IActionResult Index()
+        //{
+        //    //this is simply how I created test data
+        //    //var diaryEntries = new List<DiaryEntry>();
+        //    //diaryEntries.Add(new DiaryEntry() { Created = DateTime.Now, Text = "Just feel like coding today haha" });
+        //    //diaryEntries.Add(new DiaryEntry() { Created = DateTime.Now.AddDays(-1),
+        //    //    Text = "Rained all day great thats a happy person lolol" });
+        //    //diaryEntries.Add(new DiaryEntry() { Created = DateTime.Now.AddDays(-2), Text = "What a fun project" });
 
-            var options = new DbContextOptions<ApplicationDbContext>();
-            using (var context = new ApplicationDbContext(options))
-            {
-                var UserId = User.Identity.GetUserId();
+        //    var options = new DbContextOptions<ApplicationDbContext>();
+        //    using (var context = new ApplicationDbContext(options))
+        //    {
+        //        var UserId = User.Identity.GetUserId();
 
-                //linq query to get entries from database and see newest
-                var diaryEntries = context.DiaryEntries
-                    .Where(d => d.UserId == UserId)
-                    .OrderByDescending(d => d.Created);
+        //        //linq query to get entries from database and see newest
+        //        var diaryEntries = context.DiaryEntries
+        //            .Where(d => d.UserId == UserId)
+        //            .OrderByDescending(d => d.Created);
 
-                ViewBag.DiaryEntries = diaryEntries.ToList();
-            }
+        //        ViewBag.DiaryEntries = diaryEntries.ToList();
+        //    }
 
-            return View("Diary");
-        }
+        //    return View("Diary");
+        //}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Index(AddDiaryEntryViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                //if the model has errors it will return the viewModel
-                //which will display the form with the current datum in it
-                return View("Diary", model);
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Index(AddDiaryEntryViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        //if the model has errors it will return the viewModel
+        //        //which will display the form with the current datum in it
+        //        return View("Diary", model);
+        //    }
 
-            var diaryEntry = new DiaryEntry()
-            {
-                Id = Guid.NewGuid(),
-                UserId = User.Identity.GetUserId(),
-                Created = DateTime.Now,
-                Text = model.Text
-            };
+        //    var diaryEntry = new DiaryEntry()
+        //    {
+        //        Id = Guid.NewGuid(),
+        //        UserId = User.Identity.GetUserId(),
+        //        Created = DateTime.Now,
+        //        Text = model.Text
+        //    };
 
-            var options = new DbContextOptions<ApplicationDbContext>();
+        //    var options = new DbContextOptions<ApplicationDbContext>();
 
-            using (var context = new ApplicationDbContext(options))
-            {
-                context.DiaryEntries.Add(diaryEntry);
-                context.SaveChanges();
-            }
+        //    using (var context = new ApplicationDbContext(options))
+        //    {
+        //        context.DiaryEntries.Add(diaryEntry);
+        //        context.SaveChanges();
+        //    }
 
-            //The below old code simple added to the index view only the entry that was just created
+        //    //The below old code simple added to the index view only the entry that was just created
 
-            //    var diaryEntries = new List<DiaryEntry>();
-            //diaryEntries.Add(new DiaryEntry { Created = DateTime.Now, Text = model.Text });
+        //    //    var diaryEntries = new List<DiaryEntry>();
+        //    //diaryEntries.Add(new DiaryEntry { Created = DateTime.Now, Text = model.Text });
 
-            //ViewBag.DiaryEntries = diaryEntries;
+        //    //ViewBag.DiaryEntries = diaryEntries;
 
-            //ToDo: storing diary entry in database
+        //    //ToDo: storing diary entry in database
 
-            return RedirectToAction("Index");
-        }
+        //    return RedirectToAction("Index");
+        //}
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                return View("Diary");
+            }
             return View();
         }
 
